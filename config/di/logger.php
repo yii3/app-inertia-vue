d6702b5adeab1e73545fdcb65ca58722f1832d03
Wilmer Arambula <terabytesoftw@gmail.com>
2026-09-05 19:05:20 -0400

    refactor(config): use automatic debugger configuration and parameter-based extensions; remove the Home action instrumentation demo and its auxiliary interfaces.

--- config/di/logger.php
+++ config/di/logger.php
@@ -4,13 +4,14 @@ declare(strict_types=1);
 
 use Psr\Log\LoggerInterface;
 use Yiisoft\Definitions\ReferencesArray;
-use Yiisoft\Log\{Logger, StreamTarget};
+use Yiisoft\Log\Logger;
 
+/** @var array{'yiisoft/log': array{targets: array<string, class-string>}} $params */
 return [
     LoggerInterface::class => [
         'class' => Logger::class,
         '__construct()' => [
-            'targets' => ReferencesArray::from([StreamTarget::class]),
+            'targets' => ReferencesArray::from($params['yiisoft/log']['targets']),
         ],
     ],
 ];
