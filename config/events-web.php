4042caeece4685e067c963ba6ab8eef67a6657e2
Wilmer Arambula <42547589+terabytesoftw@users.noreply.github.com>
2026-09-21 08:45:00 -0400

    refactor: remove unused ViteCollector and VitePanel references from configuration files. (#11)

--- config/events-web.php
+++ config/events-web.php
@@ -2,9 +2,4 @@
 
 declare(strict_types=1);
 
-use PHPForge\Vite\Debug\ViteCollector;
-use PHPForge\Vite\Event\AssetsResolved;
-
-return [
-    AssetsResolved::class => [ViteCollector::class],
-];
+return [];
